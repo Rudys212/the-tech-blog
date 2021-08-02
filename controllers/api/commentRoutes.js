2ed2d3f742596858fe33f450d37a494a71178f54
Rudy Sanchez <rudys212@gmail.com>
2021-08-02 01:33:37 -0700

    models almost completed

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Post } = require('../../models');
+const { User } = require('../../models');
 
 router.post('/', async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newComment = await Comment.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newComment);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -16,19 +17,19 @@ router.post('/', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const postedComment = await Post.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (![postedComment]) {
+      res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(postedComment);
   } catch (err) {
     res.status(500).json(err);
   }
