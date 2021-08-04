f12a398710f688d1dd216d7e73040864ba5c0a3d
Rudy Sanchez <rudys212@gmail.com>
2021-08-04 01:32:50 -0700

    added post route in homeroutes.js

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -4,8 +4,22 @@ const sequelize = require('.../config/connection');
 
 router.get('/', (req, res)=>{
     Post.findAll({
-attributes:[
-    
-]
+attributes:['id','title', 'post_body', 'date_created', 'user_id'],
+include: [{
+    model: User,
+    as:'user',
+    attributes:['username']
+},{
+    model: Comment,
+    as:'comment',
+    attributes:['id', 'user_id', 'comment_body']
+},
+],
     })
 })
+
+.then(postData) => {
+    const posts = postData.map((posts)=> posts.get({
+        plain: true
+    }));
+}
