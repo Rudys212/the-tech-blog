54986ea2bb2bf3447a5a10808bebee9334d75738
Rudy Sanchez <rudys212@gmail.com>
2021-08-04 19:32:11 -0700

    updated homeroute.js

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,25 +1,39 @@
 const router = require('express').Router();
-const { Post, Comment, User};
-const sequelize = require('.../config/connection');
+const { Post, Comment, User } = require('../models');
+// const sequelize = require('../config/connection');
 
-router.get('/', (req, res)=>{
-    Post.findAll({
-attributes:['id','title', 'post_body', 'date_created', 'user_id'],
-include: [{
-    model: User,
-    as:'user',
-    attributes:['username']
-},{
-    model: Comment,
-    as:'comment',
-    attributes:['id', 'user_id', 'comment_body']
-},
-],
-    })
-})
+router.get('/', async (req, res) => {
+  console.log('check');
+  try {
+    const postData = await Post.findAll({
+      //   attributes: ['id', 'title', 'post_body', 'date_created', 'user_id'],
+      //   include: [
+      //     {
+      //       model: User,
+      //       as: 'user',
+      //       attributes: ['username'],
+      //     },
+      //     {
+      //       model: Comment,
+      //       as: 'comment',
+      //       attributes: ['id', 'user_id', 'comment_body'],
+      //     },
+      //   ],
+    });
 
-.then(postData) => {
-    const posts = postData.map((posts)=> posts.get({
-        plain: true
-    }));
-}
+    //   .then((postData) => {
+    const posts = postData.map((posts) =>
+      posts.get({
+        plain: true,
+      })
+    );
+    console.log(posts);
+    res.render('homepage', {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
